0193fe563b48021fa9f8e0ebf9448b20b5d78907
Michael Lesniak <mlesniak@micromata.de>
2015-11-28 11:31:57 +0100

    Remove unused field

--- src/main/java/com/mlesniak/sap/playground/MainController.java
+++ src/main/java/com/mlesniak/sap/playground/MainController.java
@@ -1,7 +1,5 @@
 package com.mlesniak.sap.playground;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/rest/text")
 public class MainController {
-    private static final Logger LOG = LoggerFactory.getLogger(MainController.class);
-
     @Autowired
     private TextService textService;
 
