b0af302a3290ca77ebcf7cf644a2ffbc38578bc9
Michael Lesniak <mlesniak@micromata.de>
2015-11-28 11:31:37 +0100

    Add a /version endpoint for git information

---
+++ src/main/java/com/mlesniak/sap/playground/VersionController.java
@@ -0,0 +1,39 @@
+package com.mlesniak.sap.playground;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Show version information provided by the git-properties plugin.
+ *
+ * @author Michael Lesniak (mlesniak@micromata.de)
+ */
+@RestController
+@RequestMapping("/rest/")
+public class VersionController {
+    @RequestMapping("/version")
+    public Map<String, String> version() throws IOException {
+        return getVersionInformation();
+    }
+
+    private Map<String, String> getVersionInformation() throws IOException {
+        Map<String, String> info = new HashMap<>();
+        InputStream is = VersionController.class.getResourceAsStream("/git.properties");
+        if (is == null) {
+            return info;
+        }
+        Properties properties = new Properties();
+        properties.load(is);
+
+        for (String key : properties.stringPropertyNames()) {
+            info.put(key, properties.getProperty(key));
+        }
+        return info;
+    }
+}
