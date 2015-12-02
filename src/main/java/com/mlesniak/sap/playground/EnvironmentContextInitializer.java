42862134a6f3902c80f8a2d211925bc6a6ac93f2
Michael Lesniak <mlesniak@micromata.de>
2015-12-02 22:14:14 +0100

    Formatting

--- src/main/java/com/mlesniak/sap/playground/EnvironmentContextInitializer.java
+++ src/main/java/com/mlesniak/sap/playground/EnvironmentContextInitializer.java
@@ -6,49 +6,40 @@ import org.springframework.context.ApplicationContextInitializer;
 import org.springframework.context.ConfigurableApplicationContext;
 
 /**
- * Detects the current landscape the application is run on and initializes the Spring {@link ApplicationContext} accordingly.
- * 
+ * Detects the current landscape the application is run on and initializes the Spring
+ * {@link ApplicationContext} accordingly.
+ *
  * @see ApplicationContextInitializer
  */
-public class EnvironmentContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext>
-{
-	private static final Logger logger = LoggerFactory.getLogger(EnvironmentContextInitializer.class);
-	
-	public static enum RuntimeEnvironment { DEV, PRODUCTION };
-	
-	/*
-	 * (non-Javadoc)
-	 * @see org.springframework.context.ApplicationContextInitializer#initialize(org.springframework.context.ConfigurableApplicationContext)
-	 */
-	@Override
-	public void initialize(ConfigurableApplicationContext applicationContext)
-	{
-		RuntimeEnvironment stage = getEnvironment();
-		
-		applicationContext.getEnvironment().setActiveProfiles(stage.name().toLowerCase());
-	}
-	
-	/**
-	 * Returns the {@link RuntimeEnvironment} the application runs in. Defaults to <code>CLOUD</code>.
-	 * 
-	 * @return The {@link RuntimeEnvironment} the application runs in
-	 */
-	public static RuntimeEnvironment getEnvironment()
-	{
-		RuntimeEnvironment retVal = RuntimeEnvironment.PRODUCTION;
-		
-		final String landscape = System.getenv("HC_LANDSCAPE"); // NEO
-		
-		if (landscape == null)
-		{
-			retVal = RuntimeEnvironment.DEV;
-		}
-		
-		if (logger.isInfoEnabled())
-		{
-			logger.info("Application running in '{}' environment", retVal.name().toLowerCase());			
-		}
-		
-		return retVal;
-	}
+public class EnvironmentContextInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+    private static final Logger logger = LoggerFactory.getLogger(EnvironmentContextInitializer.class);
+
+    public enum RuntimeEnvironment {
+        DEV,
+        PRODUCTION
+    }
+
+    @Override
+    public void initialize(ConfigurableApplicationContext applicationContext) {
+        RuntimeEnvironment stage = getEnvironment();
+        applicationContext.getEnvironment().setActiveProfiles(stage.name().toLowerCase());
+    }
+
+    /**
+     * Returns the {@link RuntimeEnvironment} the application runs in. Defaults to <code>CLOUD</code>.
+     *
+     * @return The {@link RuntimeEnvironment} the application runs in
+     */
+    public static RuntimeEnvironment getEnvironment() {
+        RuntimeEnvironment retVal = RuntimeEnvironment.PRODUCTION;
+
+        // HC_LANDSCAPE is set on HCP-based environments.
+        final String landscape = System.getenv("HC_LANDSCAPE");
+        if (landscape == null) {
+            retVal = RuntimeEnvironment.DEV;
+        }
+
+        logger.info("Application running in '{}' environment", retVal.name().toLowerCase());
+        return retVal;
+    }
 }
